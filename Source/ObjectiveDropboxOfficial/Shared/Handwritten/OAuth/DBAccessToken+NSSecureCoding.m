edb6fc19ff171dcace5e3f491f2236c7fbd726a0
Götz Fabian <goetz@ulysses.app>
2022-02-08 13:36:14 +0100

    Removed obsolete code.

--- Source/ObjectiveDropboxOfficial/Shared/Handwritten/OAuth/DBAccessToken+NSSecureCoding.m
+++ Source/ObjectiveDropboxOfficial/Shared/Handwritten/OAuth/DBAccessToken+NSSecureCoding.m
@@ -8,26 +8,11 @@
 @implementation DBAccessToken (NSSecureCoding)
 
 + (DBAccessToken *)createTokenFromData:(NSData *)data {
-  DBAccessToken *token = nil;
-  if (@available(iOS 11.0, macOS 10.13, *)) {
-    token = [NSKeyedUnarchiver unarchivedObjectOfClass:[DBAccessToken class] fromData:data error:NULL];
-  } else {
-    id object = [NSKeyedUnarchiver unarchiveObjectWithData:data];
-    if ([object isKindOfClass:[DBAccessToken class]]) {
-      token = object;
-    }
-  }
-  return token;
+  return [NSKeyedUnarchiver unarchivedObjectOfClass:[DBAccessToken class] fromData:data error:NULL];
 }
 
 + (NSData *)covertTokenToData:(DBAccessToken *)token {
-  NSData *data = nil;
-  if (@available(iOS 11.0, macOS 10.13, *)) {
-    data = [NSKeyedArchiver archivedDataWithRootObject:token requiringSecureCoding:YES error:NULL];
-  } else {
-    data = [NSKeyedArchiver archivedDataWithRootObject:token];
-  }
-  return data;
+  return [NSKeyedArchiver archivedDataWithRootObject:token requiringSecureCoding:YES error:NULL];
 }
 
 @end
